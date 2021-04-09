086a7d3a8d9a53432ff9de73d083808b7b72c57d
Ganesh H <43777439+ganeshh123@users.noreply.github.com>
2021-04-09 07:43:19 +0100

    init project

---
+++ Refresh Lists/Refresh Lists/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Refresh Lists
+//
+//  Created by Ganesh H on 09/04/2021.
+//  Copyright © 2021 Ganesh H. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Refresh Lists/Refresh ListsTests/Refresh_ListsTests.swift
@@ -0,0 +1,34 @@
+//
+//  Refresh_ListsTests.swift
+//  Refresh ListsTests
+//
+//  Created by Ganesh H on 09/04/2021.
+//  Copyright © 2021 Ganesh H. All rights reserved.
+//
+
+import XCTest
+@testable import Refresh_Lists
+
+class Refresh_ListsTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
