06a36f695413ce0bad2a00ce895f3dc491bccb59
Ganesh H <43777439+ganeshh123@users.noreply.github.com>
2021-04-09 07:51:53 +0100

    List Item Model

---
+++ Refresh Lists/Model/ListItem/ListItemModel.swift
@@ -0,0 +1,21 @@
+//
+//  ListItemModel.swift
+//  Refresh Lists
+//
+//  Created by Ganesh H on 09/04/2021.
+//  Copyright © 2021 Ganesh H. All rights reserved.
+//
+
+import Foundation
+
+struct ListItem {
+    
+    var title: String!
+    
+    init(title: String){
+        if(title != ""){
+            self.title = title
+        }
+    }
+    
+}
