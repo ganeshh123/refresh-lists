d7489a75c9c8ccf0e4ada90a6a39cd5c03012392
Ganesh H <43777439+ganeshh123@users.noreply.github.com>
2021-05-12 00:46:48 +0100

    list items table view comments

--- Refresh Lists/Controllers/ListItemsTableViewController.swift
+++ Refresh Lists/Controllers/ListItemsTableViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ListItemTable.swift
+//  ListItemsTableViewController.swift
 //  Refresh Lists
 //
 //  Created by Ganesh H on 15/04/2021.
@@ -7,13 +7,18 @@
 //
 
 import Foundation
-
 import UIKit
 
 extension CheckListViewController: UITableViewDataSource, UITableViewDelegate, UITableViewDragDelegate{
     
     
+    /// Sets the number of rows for items list
+    /// - Parameters:
+    ///   - tableView: Table View
+    ///   - section: Section
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        /* Set number of rows to number of list items, otherwise zero*/
         
         if let items = currentCheckList?.items {
             return items.count
@@ -24,52 +29,80 @@ extension CheckListViewController: UITableViewDataSource, UITableViewDelegate, U
         
     }
     
+    /// Sets the view to display on each row to represent the list item
+    /// - Parameters:
+    ///   - tableView: Table View
+    ///   - indexPath: Index of item
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
         let cell = tableView.dequeueReusableCell(withIdentifier: "listItemCell") as! ListItemController
         
         if let items = currentCheckList?.items {
+            
+            /* Set up List Item View for each item */
             cell.setup(inputListItem: items[indexPath.row], checkListId: currentCheckList!.id, editing: self.isEditing, deleteListItemFunc: self.deleteListItem(listItemId:), editListItemFunc: self.showEditListItemName(listItemToEdit:))
         }
         
         return cell
     }
     
+    /// Sets the height for each list item view
+    /// - Parameters:
+    ///   - tableView: Table View
+    ///   - indexPath: Index of Item
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         return 45
     }
     
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        return
-    }
-    
+    /// Returns the list item which was dragged
+    /// - Parameters:
+    ///   - tableView: Table View
+    ///   - session: Session
+    ///   - indexPath: Index of Dragged Item
     func tableView(_ tableView: UITableView, itemsForBeginning session: UIDragSession, at indexPath: IndexPath) -> [UIDragItem] {
         let dragItem = UIDragItem(itemProvider: NSItemProvider())
         dragItem.localObject = self.currentCheckList!.items?[indexPath.row]
         return [ dragItem ]
     }
     
+    /// Moves a List Item from one position to another
+    /// - Parameters:
+    ///   - tableView: Table View
+    ///   - sourceIndexPath: Index of source position
+    ///   - destinationIndexPath: Index of destination position
     func tableView(_ tableView: UITableView, moveRowAt sourceIndexPath: IndexPath, to destinationIndexPath: IndexPath) {
         
+        /* Remove the list item from source position and insert it
+            at the destination position
+         */
         let listItemToMove = self.currentCheckList!.items?.remove(at: sourceIndexPath.row)
         self.currentCheckList!.items?.insert(listItemToMove!, at: destinationIndexPath.row)
         
+        /* Persist new order to Local Storage */
         LocalStorage.updateCheckListById(checkListId: self.currentCheckList!.id, updatedCheckList: self.currentCheckList!)
         
         DispatchQueue.main.async {
+            
+            /* Reload the list to show new order */
             self.currentCheckList = LocalStorage.checkListModels[self.checkListIndex!]
             self.listItemsTableView.reloadData()
         }
         
     }
     
+    /// Deletes the list item with a specified Id from the Check List
+    /// - Parameter listItemId: Id of the list item to delete
     func deleteListItem(listItemId: UUID ){
 
+        /* Delete the list item from the Check List in Local Storage */
         LocalStorage.deleteListItemById(checkListId: currentCheckList!.id, listItemId: listItemId, completion: { deletedItemIndex in
             
             if(deletedItemIndex == -1){
+                /* If no item found to delete, stop*/
                 return
             }else{
+                
+                /* If item deleted, then update the list view to show this change*/
                 
                 DispatchQueue.main.async {
                     self.currentCheckList = LocalStorage.checkListModels[self.checkListIndex!]
@@ -82,18 +115,26 @@ extension CheckListViewController: UITableViewDataSource, UITableViewDelegate, U
         
     }
     
+    /// Allows the user to rename a provided list item
+    /// - Parameter listItemToEdit: List Item to rename
     func showEditListItemName(listItemToEdit: ListItemModel){
         
+        /* Prepare the Text Edit View */
         var listItemToEdit = listItemToEdit
         let nameEditStoryBoard = UIStoryboard(name: "TextEditView", bundle: nil)
         let textEditView = nameEditStoryBoard.instantiateInitialViewController() as! TextEditViewController
         
-        textEditView.confirmFunction = {inputName in 
-            listItemToEdit.title = inputName
+        
+        /* Set the function to run when a new name is confirmed */
+        textEditView.confirmFunction = {inputName in
             
+            /* Set the new name and persist to local storage */
+            listItemToEdit.title = inputName
             LocalStorage.updateListItemById(checkListId: self.currentCheckList!.id, listItemId: listItemToEdit.id, updatedListItem: listItemToEdit)
             
             DispatchQueue.main.async {
+                
+                /* Update the list to show the new name */
                 self.currentCheckList = LocalStorage.checkListModels[self.checkListIndex!]
                 self.listItemsTableView.reloadData()
             }
@@ -103,6 +144,8 @@ extension CheckListViewController: UITableViewDataSource, UITableViewDelegate, U
             return
         }
         
+        
+        /* Show the Text Edit View, and style the Text field */
         self.present(textEditView, animated: true)
         textEditView.prepareTextEditInput(color: Theme.getColorFromName(colorName: self.currentCheckList!.color), currentName: listItemToEdit.title)
         
